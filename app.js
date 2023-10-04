babc38dac7537ed23497480369cf973762893ecd
tonia94731 <huiyu.tonia.chang@gmail.com>
2023-10-04 22:02:31 +0800

    feat: add update restaurant function

--- app.js
+++ app.js
@@ -88,6 +88,35 @@ app.get("/restaurants/:id/edit", (req, res) => {
     .then((restaurant) => res.render("edit", { restaurant }))
     .catch(error => console.log(error))
 });
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
+  const {
+    name,
+    name_en,
+    category,
+    image,
+    location,
+    phone,
+    google_map,
+    rating,
+    description,
+  } = req.body;
+  return Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = name;
+      restaurant.name_en = name_en;
+      restaurant.category = category;
+      restaurant.image = image;
+      restaurant.location = location;
+      restaurant.phone = phone;
+      restaurant.google_map = google_map;
+      restaurant.rating = rating;
+      restaurant.description = description;
+      return restaurant.save();
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`Express is listening on http://localhost:${port}`)
